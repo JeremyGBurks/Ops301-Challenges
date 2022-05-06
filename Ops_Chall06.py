68b58e6a240f9f28cd4f6c70edf7670920d688e5
Jeremy Burks <97761340+JeremyGBurks@users.noreply.github.com>
2022-05-05 21:20:43 -0700

    Create Ops_Chall06.py

---
+++ Ops_Chall06.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Author - Jeremy Burks
+# Last Revised - 5/05/2022
+# Purpose - Runs a bash commands from inside python3
+import socket
+import subprocess 
+# "whoami" was returning a value with a letter 'b' at the beginning of the string. I googled and found on stackoverflow that the b meant python was assuming the value being returned from the command was a byte data type. The solution on stack overflow was to add the .decode() method to the command to tell python that the return value would be in a string format "utf-8" is a character encoding, it's what is telling it that it is not a byte.
+username = subprocess.check_output("whoami").strip().decode("utf-8")
+ipadd = subprocess.check_output("ip a")
+hardware = subprocess.check_output("lshw -short")
+computername = socket.gethostname()
+
+print(username + " is the user on " + computername)
+print(ipadd + " is the IP info of " + computername)
+print(hardware + " is the hardware info for " + computername)
