a045bd593476874eae1b4aaf3b1dd895ebb33986
Jeremy Burks <97761340+JeremyGBurks@users.noreply.github.com>
2022-05-12 22:38:18 -0700

    Update Ops_Chall08.ps1

--- Ops_Chall08.ps1
+++ Ops_Chall08.ps1
@@ -2,5 +2,7 @@
 # Date Last Revised - 05/12/22
 # Purpose - Creates a new OU then automates adding a user to ADAC
 
+# Creates a new OU
 New-ADOrganizationalUnit -Name "GLobeX TPS Department" -ProtectedFromAccidentalDeletion $false
-
+#Creates a new user with all their info
+New-ADUser -Name "Franz Ferdinand" -GivenName "Franz" -Surname "Ferdinand" -SamAccountName "F.Ferdinand" -UserPrincipalName "ferdi@GlobeXpower.com" -Title "TPS Reporting Lead" -Company "GlobeX USA" 
